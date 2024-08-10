80c446e163cc36388f7bdd45ae4dd29ece3268ea
Sanket <mrsanket45@gmail.com>
2024-08-10 19:12:08 +0530

    "Updated cluster name in aws ecs update-service command from ${CLUSTER_NAME} to ${CLUSTER_SERVICE}"

--- script.sh
+++ script.sh
@@ -18,4 +18,4 @@ aws ecs register-task-definition --cli-input-json file://task-definition.json --
 
 REVISION=`aws ecs describe-task-definition --task-definition "${TASK_DEFINITION_NAME}" --region "${AWS_REGION}" | jq .taskDefinition.revision`
 echo "REVISION= " "${REVISION}"
-aws ecs update-service --cluster "${CLUSTER_NAME}" --service "${SERVICE_NAME}" --task-definition "${TASK_DEFINITION_NAME}":"${REVISION}" --desired-count "${DESIRED_COUNT}"
+aws ecs update-service --cluster "${CLUSTER_SERVICE}" --service "${SERVICE_NAME}" --task-definition "${TASK_DEFINITION_NAME}":"${REVISION}" --desired-count "${DESIRED_COUNT}"
